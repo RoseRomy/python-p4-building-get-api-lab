d97c1d3cfa090122c37ee46b829c057bb11a365e
Rosemary Kamau <kamaurosemary.n@gmail.com>
2025-06-19 12:45:21 +0300

    Initial commit

--- server/models.py
+++ server/models.py
@@ -8,6 +8,7 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
@@ -22,6 +23,7 @@ class Bakery(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'<Bakery {self.name}>'
+
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
